b3e5926505cc0dd723e6504582008df149581386
Joel Eliason <joel.eliason119@gmail.com>
2023-02-15 09:09:42 -0500

    Some more updates.

--- R/generics.R
+++ R/generics.R
@@ -1,5 +1,8 @@
 #' @export
 dist_to_df <- function(x,...) UseMethod("dist_to_df")
 
+#' @export
+qdist_to_df <- function(x,...) UseMethod("qdist_to_df")
+
 #'@export
 add_QuantileDist <- function(x,...) UseMethod("add_QuantileDist")
