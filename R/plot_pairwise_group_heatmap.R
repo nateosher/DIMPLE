f7be1790875b597f0fca58c669eaa64353cb5812
Joel Eliason <joel.eliason119@gmail.com>
2023-02-16 15:15:01 -0500

    Add group labels to heatmap.

--- R/plot_pairwise_group_heatmap.R
+++ R/plot_pairwise_group_heatmap.R
@@ -4,14 +4,16 @@
 #'
 #' @return ggplot heatmap
 #' @export
-plot_pairwise_group_heatmap <- function(df) {
+plot_pairwise_group_heatmap <- function(df,group_names = c(1,2)) {
   df %>% {
     ggplot(.,aes(type1,type2,fill=estimate)) +
     geom_tile() +
     anglex() +
     sig_stars() +
     scico::scale_fill_scico(palette = "vik",
-                            label = function(z) replace(z, c(1, length(z)), c("Greater in group 2 \u2193", "Greater in group 1 \u2191")),
+                            label = function(z) replace(z, c(1, length(z)), 
+                                                        c(paste0("Greater in group ",group_names[2], " \u2193"),
+                                                          paste0("Greater in group ", group_names[1], " \u2191"))),
                             breaks = round(seq(from=-max(abs(round(.$estimate,2))),
                                                to=max(abs(round(.$estimate,2))),
                                                length.out=5),2),
