f88559c06e7775cc318bb29dd6449ec68e24fdd4
Nate Osher <nateosher@gmail.com>
2023-03-13 14:09:52 -0400

    Changed function signature for GridRect

--- R/GridRect.R
+++ R/GridRect.R
@@ -7,12 +7,17 @@
 #' @param bot_left_corner_y y coordinate of the bottom left corner of the rectangle
 #' @param width width of rectangle
 #' @param height height of rectangle
-#' @param intensity Intensity of point pattern on the rectangle
+#' @param intensity Intensity of point pattern on the rectangle; either a scalar
+#' value that will be constant on the entire rectangle, or a vector of two values
+#' indicating the minimum and maximum values to be reached vertically or horizontally
+#' over the rectangle
+#' @param axis Either 'x'/'X' or 'y'/'Y'. If intensity is passed as min/max
+#' gradient, this argument specifies which axis the gradient should occur across.
 #' @return Intensity matrix of specified dimension with "rectangle" in
 #' specified location.
 #' @export
 GridRect = function(m, n, bot_left_corner_x, bot_left_corner_y, width,
-                    height, intensity){
+                    height, intensity, axis = NULL){
   intensity_mat = matrix(0, nrow = m, ncol = n)
   rect_select_tib = tidyr::expand_grid(r = 1:height, c = 1:width)
   rect_select = map(1:nrow(rect_select_tib),
