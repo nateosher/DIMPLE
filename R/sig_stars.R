7584f01a01085e0044a7b514fe720d3d58282e7f
Nate Osher <nateosher@gmail.com>
2023-03-17 09:58:59 -0400

    Added basic tests for pairwise group heatmap + altered sig_stars to decrease warnings

--- R/sig_stars.R
+++ R/sig_stars.R
@@ -5,6 +5,14 @@
 #' @return
 #' @export
 sig_stars <- function(alpha=0.05,p_values="p.adj") {
-  list(geom_point(aes(shape=ifelse(!!sym(p_values) < alpha, "dot", "no_dot")),color='red',size=2),
-       scale_shape_manual(values=c(dot=8, no_dot=NA), guide="none"))
+  list(
+      geom_point(aes(shape=ifelse(!!sym(p_values) < alpha, "dot", "no_dot"),
+                     alpha=ifelse(!!sym(p_values) < alpha, "dot", "no_dot")),
+                 color='red',size=2),
+      scale_shape_manual(values=c(dot=8, no_dot=0), guide="none"),
+      # Rather than setting the shape to be NA, which throws a warning, I
+      # set the alpha based on significance to make non-significant stars
+      # invisible
+      scale_alpha_manual(values=c(dot=1, no_dot=0), guide="none")
+  )
 }
