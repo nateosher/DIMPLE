484c2648413dcdc09cf84b82766fb12f2e73f744
Nate Osher <nateosher@gmail.com>
2023-04-04 08:40:49 -0400

    Added function for filtering MltplxExperiment

---
+++ R/filter_exp.R
@@ -0,0 +1,19 @@
+#' Given a MltplxExperiment object and a vector of ids,
+#' produces a new MltplxExperiment object containing only slides whose
+#' ids are in the id vector.
+#' @param mltplx_experiment MltplxExperiment object to be filtered
+#' @param id_list vector of ids to keep in resulting MltplxExperiment object
+#' @return New MltplxExperiment object containing only specified slides
+#' @export
+filter_exp = function(mltplx_experiment, id_list){
+  if(class(mltplx_experiment) != "MltplxExperiment")
+    stop("`mltplx_experiment` argument must be of class `MltplxExperiment`")
+  filtered_exp = mltplx_experiment
+  filtered_exp$mltplx_objects = purrr::keep(filtered_exp$mltplx_objects,
+                                            ~ .x$slide_id %in% id_list)
+  if(!is.null(filtered_exp$metadata)){
+    filtered_exp$metadata = filtered_exp$metadata %>%
+      dplyr::filter(slide_id %in% id_list)
+  }
+  return(filtered_exp)
+}
