b706c02c65c32bf976a03ff1d9992a60b37fbba9
Maria Masotti <mmasotti@umich.edu>
2023-08-21 14:52:29 -0400

    MakeDistMat can return nonsymmetric matrix

--- R/MakeDistMat.R
+++ R/MakeDistMat.R
@@ -5,8 +5,15 @@
 #' @param dist_metric A distance metric between intensities;  Any function that
 #' takes in two vectors of equal length and produces a scalar
 #' @return Distance matrix with rows/columns named appropriately
-MakeDistMat = function(intensity_mat, dist_metric){
-  col_combos = ncol(intensity_mat) %>% combn(2) %>% t()
+MakeDistMat = function(intensity_mat, dist_metric, symmetric=T){
+  if(symmetric==F){
+    col_combos = tibble(x1=1:ncol(intensity_mat),x2=1:ncol(intensity_mat)) %>% 
+      expand(x1, x2) %>%
+      filter(x1!=x2) %>% 
+      as.matrix()
+  }else{
+    col_combos = ncol(intensity_mat) %>% combn(2) %>% t()
+  }
   dist_mat = matrix(NA, nrow = ncol(intensity_mat), ncol = ncol(intensity_mat))
   dists = map_dbl(1:nrow(col_combos), \(i){
     dist_metric(intensity_mat[,col_combos[i, 1]],
